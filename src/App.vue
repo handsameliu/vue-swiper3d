691137cd8dd58f7b6d75a9c616d592bfb6e309fd
handsameliu <wei.liu@semioe.com>
2017-11-29 12:41:57 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+    <!-- <img src="./assets/logo.png">
+    <router-view/> -->
+    <swiper3d :list="list" :cur-width="curWidth" :all-width="allWidth" :all-height="allHeight" :cur-height="curHeight" :scale="0.8"></swiper3d>
+  </div>
+</template>
+
+<script>
+import swiper3d from './components/Swiper-3d'
+
+function getViewportSize () {
+  return {
+    width: window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth,
+    height: window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight
+  }
+}
+let SIZE = getViewportSize()
+export default {
+  name: 'app',
+  components: {swiper3d},
+  data () {
+    return {
+      allWidth: SIZE.width,
+      curWidth: SIZE.width / 1.2,
+      allHeight: SIZE.height / 4.27,
+      curHeight: SIZE.height / 4.27 * 0.95,
+      list: [
+        {content: 1, color: 'red'},
+        {content: 2, color: 'green'},
+        {content: 3, color: 'blue'},
+        {content: 4, color: '#66CC66'},
+        {content: 5, color: '#666666'}
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  /* margin-top: 60px; */
+}
+</style>
---
+++ src/components/Swiper-3d.vue
@@ -0,0 +1,154 @@
+<template>
+  <div class="swiper-box">
+    <div class="swiper-list">
+      <div v-for="(item, index) in items" :key="index" class="item" :style="{width:item.width+'px',height:item.height+'px',left:item.left+'px',bottom:item.bottom+'px','z-index':item.zIndex,opacity:item.opacity,'background-color':rlist[index].color}">
+          <span :style="{'line-height':item.height+'px'}">{{rlist[index].content}}</span>
+      </div>
+    </div>
+    <div class="swiper-point">
+      <div class="point" v-for="(item, index) in items" :key="index" :class="globalKey==index&&'on'"></div>
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'swiper-3d',
+    props: ['list', 'allWidth', 'allHeight', 'curHeight', 'curWidth', 'scale'],
+    data () {
+      let items = []
+      let rlist = this.copyArr(this.list)
+      let level = Math.floor(this.list.length / 2)
+      if (this.list.length % 2 === 0) {
+        let center = this.list[0]
+        rlist.push(Object.assign({}, center))
+      }
+      let lefts = rlist.slice(0, level)
+      let rights = rlist.slice(level)
+      let that = this
+      let leftGap = (this.allWidth - this.curWidth) / 2
+      let gap = leftGap / level
+      lefts.forEach((e, i) => {
+        let obj = {}
+        console.log('left', e.content)
+        obj.content = e.content
+        // obj.left = i * gap;
+        obj.left = i * gap - that.curWidth + leftGap + gap * 2.5
+        obj.zIndex = i + 1
+        obj.opacity = 1 / (level + 1 - i)
+        obj.width = that.curWidth * Math.pow(that.scale, level - i)
+        obj.height = that.curHeight * Math.pow(that.scale, level - i)
+        obj.bottom = (that.allHeight - obj.height) / 2
+        obj.key = items.length
+        items.push(obj)
+      })
+      rights.forEach((e, i) => {
+        let obj = {}
+        console.log('right', e.content)
+        obj.content = e.content
+        obj.width = that.curWidth * Math.pow(that.scale, i)
+        obj.height = that.curHeight * Math.pow(that.scale, i)
+        // obj.left = that.allWidth - (level - i) * gap - obj.width
+        obj.left = leftGap + i * (that.curWidth + (level - i) * gap - (gap / 2))
+        obj.zIndex = level - i + 1
+        obj.opacity = 1 / (i + 1)
+        obj.bottom = (that.allHeight - obj.height) / 2
+        obj.key = items.length
+        items.push(obj)
+      })
+      return {
+        items: items,
+        rlist: rlist,
+        timer: null,
+        dir: 'right',
+        globalKey: level,
+        level: level
+      }
+    },
+    created () {
+      this.setTimer()
+    },
+    methods: {
+      setTimer () {
+        let that = this
+        this.clearTimer()
+        function cb () {
+          that.timer = setTimeout(() => {
+            if (that.dir === 'right') {
+              let pop = that.items.shift()
+              that.items.push(pop)
+              that.globalKey = that.items[that.level].key
+            } else {
+              let pop = that.items.pop()
+              that.items.unshift(pop)
+              that.globalKey = that.items[that.level].key
+            }
+            cb()
+          }, 2000)
+        }
+        cb()
+      },
+      clearTimer () {
+        if (this.timer) {
+          clearTimeout(this.timer)
+        }
+      },
+      copyArr (arr) {
+        return arr.map((e) => {
+          if (typeof e === 'object') {
+            return Object.assign({}, e)
+          } else {
+            return e
+          }
+        })
+      }
+    }
+  }
+</script>
+<style scoped>
+    .swiper-box{
+        width:100%;
+        overflow: hidden;
+        opacity: 0.7;
+        background-color: #B9E1FE;
+    }
+    .swiper-list{
+        width:100%;
+        height:7.4rem;
+        position:relative;
+        margin:0 auto;
+    }
+    .item{
+        box-sizing:border-box;
+        position:absolute;
+        display:block;
+        background-color:blue;
+        color:#fff;
+        text-align:center;
+        transition: all .8s ease;
+        border-radius:5px;
+        box-shadow: 0px 0px 10px #999;
+    }
+    .item span{
+        transition:inherit;
+        font-size:60px;
+    }
+    .swiper-point{
+        width: 100%;
+        height: 0.8rem;
+        line-height: 0.3rem;
+        text-align: center;
+        margin-top: -0.1rem;
+    }
+    .swiper-point .point{
+        display:inline-block;
+        width:0.2rem;
+        height: 0.2rem;
+        border-radius: 50%;
+        opacity: 0.7;
+        background-color: #FFFFFF;
+        margin-right:0.3rem;
+    }
+    .swiper-point .point.on{
+        background-color:#F99A27;
+    }
+</style>
